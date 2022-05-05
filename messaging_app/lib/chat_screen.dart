6c2d71d23892ddd3b5781fd6777c108ab3583884
Ronak Parmar <97673784+ronakj8617@users.noreply.github.com>
2022-05-05 23:43:42 +0530

    able to write into and read from a json file having issues with writing it properly

--- messaging_app/lib/chat_screen.dart
+++ messaging_app/lib/chat_screen.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:messaging_app/models/message_model.dart';
 import 'package:messaging_app/models/user_model.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ChatScreen extends StatefulWidget {
   final User user;
@@ -15,30 +19,179 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  static const String kFileName = 'myJsonFile.json';
+
+  bool _fileExists = false;
+  File? _filePath;
+
+  // First initialization of _json (if there is no json in the file)
+  Map<String, dynamic> _json = {};
+  late String _jsonString;
+
+  Future<String> get _localPath async {
+    final directory = Directory("/Users/ronak/Documents/TextApp" + kFileName);
+    // final directory = await getApplicationDocumentsDirectory();
+
+    if (directory.exists() == false) {
+      directory.create();
+    }
+
+    // log(directory.path.toString());
+    // log(directory.path);
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/$kFileName');
+  }
+
+  void _writeJson(String key, dynamic value) async {
+    // Initialize the local _filePath
+    //final _filePath = await _localFile;
+
+    //1. Create _newJson<Map> from input<TextField>
+    Map<String, dynamic> _newJson = {key: value};
+    print('1.(_writeJson) _newJson: $_newJson');
+
+    //2. Update _json by adding _newJson<Map> -> _json<Map>
+    _json.addAll(_newJson);
+    print('2.(_writeJson) _json(updated): $_json');
+
+    //3. Convert _json ->_jsonString
+    _jsonString = jsonEncode(_json);
+    print('3.(_writeJson) _jsonString: $_jsonString\n - \n');
+
+    //4. Write _jsonString to the _filePath
+    _filePath?.writeAsString(_jsonString);
+  }
+
+  void _readJson() async {
+    // Initialize _filePath
+    _filePath = await _localFile;
+
+    // 0. Check whether the _file exists
+    _fileExists = (await _filePath?.exists())!;
+    print('0. File exists? $_fileExists');
+
+    // If the _file exists->read it: update initialized _json by what's in the _file
+    if (_fileExists) {
+      try {
+        //1. Read _jsonString<String> from the _file.
+        _jsonString = (await _filePath?.readAsString())!;
+        // log('Read');
+        print('1.(_readJson) _jsonString: $_jsonString');
+
+        //2. Update initialized _json by converting _jsonString<String>->_json<Map>
+        _json = jsonDecode(_jsonString);
+        print('2.(_readJson) _json: $_json \n - \n');
+      } catch (e) {
+        // Print exception errors
+        print('Tried reading _file error: $e');
+        // If encountering an error, return null
+      }
+    }
+  }
+
+  List _items = [];
+
+  // Fetch content from the json file
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/chats.json');
+    log('Response: ' + response);
+
+    final test = rootBundle.loadString('assets/chats.json');
+    log('test');
+
+    Directory appDocDirectory = await getApplicationDocumentsDirectory();
+    log('Parent: ' + appDocDirectory.parent.path);
+    new Directory(appDocDirectory.path + '/chats')
+        .create(recursive: true)
+        .then((Directory directory) {
+      // print('Path of New Dir: ' + directory.path);
+    });
+
+    var file = File(appDocDirectory.path + '/chats/chats.json');
+
+    log('File: ' + file.toString());
+    final SecurityContext context = SecurityContext.defaultContext;
+    // String crtPath = await _getLocalFile("client.crt");
+    // context.setTrustedCertificates(file.path);
+
+    file.readAsString().then((value) => log(value));
+
+    // final data = await json.decode(response);
+    setState(() {
+      // _items = data["items"];
+    });
+
+    List dataItems = [];
+    List check = [];
+    for (var i = 0; i < chats.length; i++) {
+      // if (chats[i].sender.id == widget.user.id && chats[i].unread) {
+
+      // log('message');
+      Message tempMesage = Message(
+          sender: chats[i].sender,
+          time: chats[i].time,
+          text: chats[i].text,
+          isLiked: chats[i].isLiked,
+          unread: false);
+      // chats.removeAt(i);
+      // log(chats[i].text);
+      dataItems.add(tempMesage);
+      var test = tempMesage.toJson(tempMesage);
+      check.add(jsonEncode(test));
+      // log(check);
+
+      // chats.insert(chats.length, tempMesage);
+      // chats[i] = tempMesage;
+      // chats[i].unread=false;
+
+    }
+
+    // file.writeAsString(check.add());
+   check.forEach((element) { file
+        .writeAsString(element)
+        .then((value) => log(value.toString()))
+        .onError((error, stackTrace) => print(error));});
+
+
+    // file.readAsString().then((value) => log(value));
+    // _items.forEach((element) {log(element['id']);});
+  }
+
   @override
   void initState() {
     super.initState();
 
-    for (var i = 0; i < chats.length; i++) {
-      if (chats[i].sender.id == widget.user.id && chats[i].unread) {
-        log('message');
-        Message tempMesage = Message(
-            sender: chats[i].sender,
-            time: chats[i].time,
-            text: chats[i].text,
-            isLiked: chats[i].isLiked,
-            unread: false);
-        chats.removeAt(i);
-        // log(chats[i].text);
-        var test=tempMesage.toJson(tempMesage);
-        var check=jsonEncode(test);
-        log(check);
+    readJson();
+    initializeFiles();
+    print('0. Initialized _json: $_json');
+    _writeJson('key', 'value');
+    _readJson();
 
-         chats.insert(chats.length , tempMesage);
-        // chats[i] = tempMesage;
-        // chats[i].unread=false;
-      }
-    }
+    // for (var i = 0; i < chats.length; i++) {
+    //   if (chats[i].sender.id == widget.user.id && chats[i].unread) {
+    //     // log('message');
+    //     Message tempMesage = Message(
+    //         sender: chats[i].sender,
+    //         time: chats[i].time,
+    //         text: chats[i].text,
+    //         isLiked: chats[i].isLiked,
+    //         unread: false);
+    //     chats.removeAt(i);
+    //     // log(chats[i].text);
+    //     var test = tempMesage.toJson(tempMesage);
+    //     var check = jsonEncode(test);
+    //     // log(check);
+
+    //     chats.insert(chats.length, tempMesage);
+    //     // chats[i] = tempMesage;
+    //     // chats[i].unread=false;
+    //   }
+    // }
   }
 
   @override
@@ -199,10 +352,15 @@ class _ChatScreenState extends State<ChatScreen> {
                 ),
               ),
             ),
+            Text(_filePath.toString()),
             _buildMessageComposer(),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> initializeFiles() async {
+    _filePath = await _localFile;
   }
 }
