67cd8184589ca0510e68d6e2892a2ec934afb52f
Ronak Parmar <97673784+ronakj8617@users.noreply.github.com>
2022-05-13 00:01:28 +0530

    able to write a model class into firebase Raven 1.1.1

--- raven/lib/home_screen.dart
+++ raven/lib/home_screen.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:raven/models/message_model.dart';
+import 'package:raven/models/user_model.dart';
 import 'package:raven/widgets/category_selector.dart';
 import 'package:raven/widgets/favourite_contacts.dart';
 import 'package:raven/widgets/recent_chats.dart';
@@ -33,10 +35,10 @@ class _HomeScreenState extends State<HomeScreen> {
   Future<void> firebaseInit() async {
         await Firebase.initializeApp();
 
-    if (Firebase.apps.length == 0) {
-      // await Firebase.initializeApp(
-          // options: DefaultFirebaseOptions.currentPlatform);
-    } // Firebase.initializeApp().onError((error, stackTrace) => (error));
+    // if (Firebase.apps.length == 0) {
+    //   // await Firebase.initializeApp(
+    //       // options: DefaultFirebaseOptions.currentPlatform);
+    // } // Firebase.initializeApp().onError((error, stackTrace) => (error));
 
     // FirebaseApp secondaryApp = Firebase.app('raven (ios)');
     // FirebaseDatabase database = FirebaseDatabase.instanceFor(app: secondaryApp);
@@ -44,10 +46,10 @@ class _HomeScreenState extends State<HomeScreen> {
     DatabaseReference ref = FirebaseDatabase.instance.ref("raven (ios)");
     var ios = DefaultFirebaseOptions.ios;
 
-    await ref.set({
-      "name": "John",
-      "age": 18,
-      "address": {"line1": "100 Mountain View"}
+    User user = User(id: 001, name: 'name', imageUrl: 'imageUrl');
+    Message message= Message(sender:user, time: 'time', text: 'text', isLiked: true, unread: true);
+
+    await ref.child(Timeline.now.toString()).set({'user':message.toJson(message)
     });
   }
 
@@ -64,7 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
           onPressed: () {},
         ),
         title: const Text(
-          'Raven 1.1.0',
+          'Raven 1.1.1',
           style: TextStyle(
             fontSize: 28.0,
             fontWeight: FontWeight.bold,
