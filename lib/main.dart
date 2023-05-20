1cd9cb20409a654076e4e6bbdde49f557d5a990f
Ronak Parmar <ronakp447450@gmail.com>
2023-05-20 19:24:58 +0530

    Testing the results; website's been running pretty fine
    Signed-off-by: Ronak Parmar <ronakp447450@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 10;
+  int _counter = 100;
 
   void _incrementCounter() {
     setState(() {
