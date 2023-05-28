ca52dd424137784caf14e5d431947d3ab0860dba
Ronak Parmar <ronakp447450@gmail.com>
2023-05-28 12:37:15 +0530

    Adding apprite
    Signed-off-by: Ronak Parmar <ronakp447450@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,6 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -56,16 +58,33 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 100;
+  Client client = Client();
 
+  void createUser() async{   
+      
+    final client = Client()
+                  .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+                  .setProject('Raven');               // Your project ID
+
+    final account = Account(client);
+
+    final session = await account.createPhoneSession(
+      userId: ID.unique(),
+      phone: '+917283844571'
+    );
+    
+  }
   void _incrementCounter() {
-    setState(() {
+    setState((){
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter+= 1000;
+      _counter+= 1000;      
     });
+
+    createUser();
   }
 
   @override
